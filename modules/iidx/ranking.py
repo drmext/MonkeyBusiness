57b720da3fc01cad94d9b1f658e7d6260201d3d6
mee19mc <85102106+mee19mc@users.noreply.github.com>
2025-08-06 21:02:31 +0800

    Update ranking.py
    Add Models
    - JDJ and lower

--- modules/iidx/ranking.py
+++ modules/iidx/ranking.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Request, Response
 from core_common import core_process_request, core_prepare_response, E
 
 router = APIRouter(prefix="/local", tags=["local"])
-router.model_whitelist = ["LDJ", "KDZ", "JDZ"]
+router.model_whitelist = ["LDJ", "KDZ", "JDZ", "JDJ", "I00", "HDD", "GLD", "FDD", "ECO", "E11", "D01", "C02"]
 
 
 @router.post("/{gameinfo}/ranking/getranker")
